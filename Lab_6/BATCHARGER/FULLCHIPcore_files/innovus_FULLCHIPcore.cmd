6801b5dea8dad503a7061501893a88dbced29ea3
Kristian Goystdal <krisgoy100@gmail.com>
2025-01-16 12:56:00 +0000

    Add design check reports and HTML summaries for BATCHARGERcore

--- Lab_6/BATCHARGER/FULLCHIPcore_files/innovus_FULLCHIPcore.cmd
+++ Lab_6/BATCHARGER/FULLCHIPcore_files/innovus_FULLCHIPcore.cmd
@@ -8,15 +8,15 @@ set init_gnd_net dgnd
 init_design
 getIoFlowFlag
 setIoFlowFlag 0
-floorPlan -site core_2800 -s 802.4 320.8 5.6 5.6 5.6 5.6
+floorPlan -site core_2800 -s 852.8 352.0 5.6 5.6 5.6 5.6
 uiSetTool select
 getIoFlowFlag
 
 setDrawView fplan
-placeInstance BATCHctr 15.6 15.6 R270
-placeInstance BATCHpower 66.3 15.6
-placeInstance BATCHbg 677.1 15.6 
-placeInstance BATCHsaradc 677.1 115.6 R270
+placeInstance BATCHctr 20.6 30.6 R270
+placeInstance BATCHpower 86.4 30.6
+placeInstance BATCHbg 712.2 30.6 
+placeInstance BATCHsaradc 712.2 130.6 R270
 setDrawView place
 setDrawView fplan
 set sprCreateIeRingOffset 1.0
@@ -25,35 +25,49 @@ set sprCreateIeRingJogDistance 1.0
 set sprCreateIeRingLayers {}
 set sprCreateIeStripeWidth 10.0
 set sprCreateIeStripeThreshold 1.0
+
 getPinAssignMode -pinEditInBatch -quiet
 setPinAssignMode -pinEditInBatch true
-editPin -pinWidth 0.2 -pinDepth 0.52 -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Right -layer 3 -spreadType center -spacing 0.4 -pin {en dvdd dgnd vbattemp}
-setPinAssignMode -pinEditInBatch false
-getPinAssignMode -pinEditInBatch -quiet
-setPinAssignMode -pinEditInBatch true
-editPin -pinWidth 0.2 -pinDepth 0.52 -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Bottom -layer 2 -spreadType center -spacing 0.4 -pin {vin pgnd sel}
-setPinAssignMode -pinEditInBatch false
-getPinAssignMode -pinEditInBatch -quiet
-setPinAssignMode -pinEditInBatch true
-editPin -pinWidth 0.2 -pinDepth 0.52 -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Top -layer 2 -spreadType center -spacing 0.4 -pin {iforcedbat vsensbat}
+editPin -pinWidth 0.2 -pinDepth 0.52 -fixOverlap 1 -spreadDirection clockwise -side Bottom -layer 2 -spreadType range -start 700.0 0.0 -end 200.0 0.0 -pin {vin {sel[3]} {sel[2]} {sel[1]} {sel[0]}}
 setPinAssignMode -pinEditInBatch false
 
+getPinAssignMode -pinEditInBatch -quiet
+setPinAssignMode -pinEditInBatch true
+editPin -USE POWER -pinWidth 2.0 -pinDepth 0.52 -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Top -layer 3 -spreadType start -spacing 0.4 -start 400.0 363.2 -pin dvdd
+setPinAssignMode -pinEditInBatch false
+
+getPinAssignMode -pinEditInBatch -quiet
+setPinAssignMode -pinEditInBatch true
+editPin -use GROUND -pinWidth 2.0 -pinDepth 0.52 -fixOverlap 1 -spreadDirection clockwise -side Top -layer 3 -spreadType range -start 600.0 360.0 -end 500.0 360.0 -pin {pgnd dgnd}
+setPinAssignMode -pinEditInBatch false
+
+getPinAssignMode -pinEditInBatch -quiet
+setPinAssignMode -pinEditInBatch true
+editPin -pinWidth 0.2 -pinDepth 0.52 -fixOverlap 1 -spreadDirection clockwise -side Right -layer 3 -spreadType range -start 864.0 363.2 -end 864.0 50.2 -pin {vsensbat iforcedbat vbattemp en}
+setPinAssignMode -pinEditInBatch false
+
+
 clearGlobalNets
-globalNetConnect agnd -type pgpin -pin agnd -instanceBasename *
+globalNetConnect dgnd -type pgpin -pin dgnd -instanceBasename *
 add_ndr -width {metal1 2.0 metal2 2.0 metal3 2.0 metal4 2.0 metal5 2.0 metal6 2.0 metal7 2.0 metal8 2.0 } -spacing {metal1 0.4 metal2 0.4 metal3 0.4 metal4 0.4 metal5 0.4 metal6 0.4 metal7 0.4 metal8 0.4 } -min_cut {via2 1 via3 1 via4 1 via5 1 via6 1 via7 1 } -add_via {VIAM1M2A VIAM2M3 VIAM3M4 VIAM4M5 VIAM5M6 VIAM6M7 VIAM7M8 genm1m2_w genm1m2a genm1m2b genm2m3_w genm2m3a genm2m3b genm3m4_w genm3m4a genm3m4b genm4m5_w genm4m5a genm4m5b genm5m6_w genm5m6a genm5m6b genm6m7_w genm6m7a genm6m7b genm7m8_w genm7m8a genm7m8b} -name pwr
-setAttribute -net avdd -non_default_rule pwr
-setAttribute -net agnd -non_default_rule pwr
+@REM setAttribute -net avdd -non_default_rule pwr
+@REM setAttribute -net agnd -non_default_rule pwr
 setAttribute -net dgnd -non_default_rule pwr
-setAttribute -net dvdd -non_default_rule pwr
-setAttribute -net pgnd -non_default_rule pwr
-setAttribute -net vin -non_default_rule pwr
-selectNet agnd
+@REM setAttribute -net dvdd -non_default_rule pwr
+@REM setAttribute -net pgnd -non_default_rule pwr
+@REM setAttribute -net vin -non_default_rule pwr
+selectNet dgnd
 routeDesign -selected
 
 setSrouteMode -viaConnectToShape { noshape }
-sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { metal1(1) metal8(8) } -blockPinTarget { nearestTarget } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -corePinTarget { firstAfterRowEnd } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { metal1(1) metal8(8) } -nets { agnd } -allowLayerChange 1 -blockPin useLef -targetViaLayerRange { metal1(1) metal8(8) }
+sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { metal1(1) metal8(8) } -blockPinTarget { nearestTarget } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -corePinTarget { firstAfterRowEnd } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { metal1(1) metal8(8) } -nets {dgnd} -allowLayerChange 1 -blockPin useLef -targetViaLayerRange { metal1(1) metal8(8) }
 placeAIO
-setAttribute -net dgnd -shield_net agnd
+setAttribute -net avdd -shield_net pgnd
+setAttribute -net agnd -shield_net dgnd
+setAttribute -net ibias1ua -shield_net agnd
+setAttribute -net ibias1ub -shield_net agnd
+setAttribute -net vrefa -shield_net agnd
+setAttribute -net vrefb -shield_net agnd
 selectNet -shield
 routeDesign -selected
 routeDesign -globalDetail
